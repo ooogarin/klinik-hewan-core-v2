b49d23daf8f1d53fc40a2d752924a1a4bb4778fe
garin-laptop <ooogarin@gmail.com>
2025-07-13 19:51:11 +0700

    feat: typo

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import userRouter from './user.route.js';
 import akunRouter from './akun.route.js';
 
 const router = express.Router();
 
-router.use('/users', userRouter);
 router.use('/akun', akunRouter);
 
 export default router;
